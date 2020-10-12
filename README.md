c0ca5b71bef3bf540c657f31cec53a5729bd3510
burhan_maseel <burhanmaseel@gmail.com>
2020-10-13 01:44:40 +0500

    Task implemented using async.js

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# flowLibrary
+# flowLibrary Version
+
+Fetch titles from provide URL(s) in the address parameter using Async.js
+
+Tested URLs:
+
+  - http://localhost:5000/I/want/title/?address=https://www.google.com&address=https://www.dawn.com/events/
+  - http://localhost:5000/I/want/title/?address=https://www.dawn.com/events/
+  - http://localhost:5000/I/want/title/?address=https://www.google.com
+
+## Installation
+
+
+```bash
+npm i
+```
+
+## Usage
+
+```
+npm start
+```
---
+++ controllers/titleControllers.js
@@ -0,0 +1,50 @@
+var async = require("async");
+const https = require("https");
+
+exports.getTitles = async (req, res) => {
+   let address = req.query.address;
+   if (!Array.isArray(req.query.address)) {
+      address = [req.query.address];
+   }
+   let titles = [];
+   async.forEachOf(address, (url, index, callback) => {
+      let title = "";
+
+      https.get(url, (resp) => {
+         let data = '';
+
+         // A chunk of data has been recieved.
+         resp.on('data', (chunk) => {
+            data += chunk;
+         });
+
+         // The whole response has been received. Print out the result.
+         resp.on('end', () => {
+            title = url + " - " + data.split("<title>").pop().split("</title>")[0];
+            titles.push(title);
+            callback();
+            if (titles.length == address.length) {
+               res.status(200).render('index', {
+                  titles: titles
+               });
+            }
+         });
+
+      }).on("error", (err) => {
+         let title = url + " - NO RESPONSE";
+         titles.push(title);
+         callback();
+         if (titles.length == address.length) {
+            res.status(200).render('index', {
+               titles: titles
+            });
+         }
+      });
+      
+   }, (err) => {
+      if (err) {
+         console.log(err);
+      }
+   })
+
+};
